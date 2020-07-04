fd67ec71ae7abaa970346dfe024a2b70ad4caa65
saykat1 <nursaykat@gmail.com>
2020-07-04 13:33:25 +0600

    Updates

---
+++ precache-manifest.95b82e5ddbc3c4313c5eb94e188c4254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a3bc5726b52da2f48577936b566ff8",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "ece249f79f3d1773deb3",
+    "url": "/stopwatch/static/css/main.1849657e.chunk.css"
+  },
+  {
+    "revision": "dc86bc886eff9fe36c83",
+    "url": "/stopwatch/static/js/2.05538fb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stopwatch/static/js/2.05538fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece249f79f3d1773deb3",
+    "url": "/stopwatch/static/js/main.bc9b02c1.chunk.js"
+  },
+  {
+    "revision": "cb2bcdb37a81d6e34a23",
+    "url": "/stopwatch/static/js/runtime-main.00413f5f.js"
+  }
+]);
